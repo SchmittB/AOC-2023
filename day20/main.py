2671d3f328c94d04672b4c3a411f7e01581ca8fa
Benjamin Schmitt <s.b3njam1n@gmail.com>
2023-12-21 17:30:27 +0100

    day20

--- day20/main.py
+++ day20/main.py
@@ -1,3 +1,4 @@
+import math
 from collections import deque
 
 
@@ -37,10 +38,11 @@ for m in modules.values():
             if m.name in em.outputs:
                 m.memory[em.name] = 'low'
 
-hi = lo = 0
-rm = {}
+modules2 = modules.copy()
 
-for press in range(1000):
+hi = lo = 0
+
+for _ in range(1000):
     lo += 1
     q = deque([("broadcaster", x, "low")
                for x in modules['broadcaster'].outputs])
@@ -71,4 +73,52 @@ for press in range(1000):
             for x in module.outputs:
                 q.append((module.name, x, outgoing))
 
-print(lo * hi)
+print("part1", lo * hi)
+
+press = 0
+(feed, ) = [
+    name for name, module in modules2.items() if "rx" in module.outputs
+]
+
+cycle_lengths = {}
+seen = {name: 0 for name, module in modules2.items() if feed in module.outputs}
+while True:
+    press += 1
+    q = deque([("broadcaster", x, "low")
+               for x in modules2['broadcaster'].outputs])
+
+    while q:
+        origin, target, pulse = q.popleft()
+
+        if target not in modules2:
+            continue
+
+        module = modules2[target]
+
+        if module.name == feed and pulse == "high":
+            seen[origin] += 1
+
+            if origin not in cycle_lengths:
+                cycle_lengths[origin] = press
+            else:
+                assert press == seen[origin] * cycle_lengths[origin]
+
+            if all(seen.values()):
+                x = 1
+                for cycle_length in cycle_lengths.values():
+                    x = x * cycle_length // math.gcd(x, cycle_length)
+                print(x)
+                exit(0)
+
+        if module.type == "%":
+            if pulse == "low":
+                module.memory = "on" if module.memory == "off" else "off"
+                outgoing = "high" if module.memory == "on" else "low"
+                for x in module.outputs:
+                    q.append((module.name, x, outgoing))
+        else:
+            module.memory[origin] = pulse
+            outgoing = "low" if all(
+                x == "high" for x in module.memory.values()) else "high"
+            for x in module.outputs:
+                q.append((module.name, x, outgoing))
